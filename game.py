7a58e8a41b316463362478de883eebae27a2539d
Sebastian Joseph <theclimax25@gmail.com>
2021-02-02 19:55:52 -0500

    changed image file

--- game.py
+++ game.py
@@ -3,21 +3,20 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_width = 800
-screen_height = 800
+screen_width = 700
+screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-sun_img = pygame.image.load('img/sun.jpg')
-background_img = pygame.image.load('img/sky.png')
+back_img = pygame.image.load('img/pixil-frame-0 (2).png')
 run = True
 while run:
-    screen.blit(background_img, (0, 0))
-    screen.blit(sun_img, (100, 100))
+    screen.blit(back_img, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
         pygame.display.update()
 
 pygame.quit()
