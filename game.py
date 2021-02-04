49148f17de441605b62a6a710923f33960ab5413
Sebastian Joseph <theclimax25@gmail.com>
2021-02-03 22:56:44 -0500

    added controls and a rectangle

--- game.py
+++ game.py
@@ -8,15 +8,33 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
+x = 50
+y = 50
+width = 40
+height = 60
+velocity = 20
 back_img = pygame.image.load('img/pixil-frame-0 (2).png')
 run = True
 while run:
     screen.blit(back_img, (0, 0))
+    pygame.time.delay(100)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
-        pygame.display.update()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        x -= velocity
+    if keys[pygame.K_SPACE]:
+        y -= velocity
+    if keys[pygame.K_s]:
+        y += velocity
+    if keys[pygame.K_d]:
+        x += velocity
+
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
 
 pygame.quit()
