40d5a6e1e37bd6a39731d2dc722b64b3943008a2
Sebastian Joseph <theclimax25@gmail.com>
2021-02-05 13:57:53 -0500

    added controls and a rectangle to test

--- game.py
+++ game.py
@@ -8,12 +8,16 @@ screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
-x = 50
-y = 50
+x = 0
+y = 577
 width = 40
 height = 60
-velocity = 20
+velocity = 10
 back_img = pygame.image.load('img/pixil-frame-0 (2).png')
+
+isJump = False
+jumpCount = 10
+
 run = True
 while run:
     screen.blit(back_img, (0, 0))
@@ -25,15 +29,22 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] and x > velocity:
         x -= velocity
-    if keys[pygame.K_SPACE]:
-        y -= velocity
-    if keys[pygame.K_s]:
-        y += velocity
-    if keys[pygame.K_d]:
-        x += velocity
-
+    if keys[pygame.K_SPACE] and y > 250 - velocity:
+        isJump = True
+    if not (isJump):
+        if keys[pygame.K_s] and y < 585 - velocity:
+            y += velocity
+        if keys[pygame.K_d] and x < 665 - velocity:
+            x += velocity
+    else:
+        if jumpCount >= -10:
+            y -= jumpCount ** 2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
     pygame.draw.rect(screen, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
 
